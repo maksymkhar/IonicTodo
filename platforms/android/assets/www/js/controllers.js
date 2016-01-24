dcf06b7fa5797be8280171be07af26a02f158d5d
Max <maksymkharuk@iesebre.com>
2016-01-24 19:31:48 +0100

    Added tasks route, tasks template, changed login button route to tasks, added menu entry for tasks template

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -175,4 +175,26 @@ angular.module('starter.controllers', [])
 
 })
 
+.controller('TodosCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = true;
+    $scope.$parent.setExpanded(true);
+    $scope.$parent.setHeaderFab('right');
+
+
+    // TODO
+
+    /*
+    $timeout(function() {
+        ionicMaterialMotion.fadeSlideIn({
+            selector: '.animate-fade-slide-in .item'
+        });
+    }, 200);
+
+    // Activate ink for controller
+    ionicMaterialInk.displayEffect();
+    */
+})
+
 ;
